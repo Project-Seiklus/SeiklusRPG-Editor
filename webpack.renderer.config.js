d0716b88120ae4b790d4ce4e25473c211f4c26ed
robin* <robin_maki@planet.moe>
2019-01-05 20:53:34 +0900

    랜더러용 webpack 설정파일 제작

---
+++ webpack.renderer.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = {
+    target: 'electron-main',
+    entry: './src/renderer/main.ts',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'renderer.js'
+    },
+    node: {
+        __dirname: false,
+        __filename: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        'scss': [
+                            'vue-style-loader',
+                            'css-loader',
+                            'sass-loader'
+                        ],
+                        'sass': [
+                            'vue-style-loader',
+                            'css-loader',
+                            'sass-loader?indentedSyntax'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
+};
