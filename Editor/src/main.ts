ce1971a623cea84c671cc427f69bd4923f2577b0
robin* <robin_maki@planet.moe>
2019-01-05 21:03:36 +0900

    기초 Vue 테스트 환경 구축

--- Editor/src/main.ts
+++ Editor/src/main.ts
@@ -11,7 +11,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../test.html"));
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
