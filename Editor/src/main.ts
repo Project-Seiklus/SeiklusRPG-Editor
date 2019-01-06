4ed560b9a0fbcc39ea19e690c491c82a0f75adac
Creta5164 <creta5164@gmail.com>
2019-01-07 01:20:01 +0900

    phaser 위치 변경 및 pixi 라이브러리 추가

--- Editor/src/main.ts
+++ Editor/src/main.ts
@@ -11,7 +11,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../test.html"));
+  mainWindow.loadFile(path.join(__dirname, "../pixi-test.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
