bebb5ee63497c0ca07866ee0b4be64276eef6261
robin* <robin_maki@planet.moe>
2019-01-08 00:01:39 +0900

    보안상 렌더러의 node 통합을 끔

--- Editor/src/main.ts
+++ Editor/src/main.ts
@@ -8,6 +8,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
+    webPreferences: {nodeIntegration: false}
   });
 
   // and load the index.html of the app.
