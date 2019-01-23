68d35e253ad64cb28755bb77204aba6300ce4cd8
Creta5164 <creta5164@gmail.com>
2019-01-23 19:05:38 +0900

    `RendererLoader.ts` 추가
    `RendererLoader`는, `CssImporter`를 사용한 스크립트를 불러오는 데 특화되어 있습니다.
    
    리소스를 불러올 준비가 끝났다면 `body`의 `display`를 `none`에서 기본값으로 돌려줍니다.

--- Editor/src/renderer/RendererLoader.ts
+++ Editor/src/renderer/RendererLoader.ts
@@ -1,14 +1,31 @@
-export default function(defaultLoader:NodeRequire) {
+export default function(rendererPath:string) {
+    
+    if (rendererPath == undefined ||
+        rendererPath == null ||
+        rendererPath == '')
+        throw new Error("렌더러의 경로를 확인할 수 없습니다.");
+    
+    let loadedPackage = require(rendererPath);
+    
+    if (loadedPackage.default == null)
+        throw new Error(`(${rendererPath}) 렌더러를 불러오지 못했습니다.`);
+    
+    loadedPackage.default();
+    
     document.body.style.display = 'none';
     
-    //TODO : 로더 작업
+    let preventOnLoad:Function = document.body.onload;
     
-    //defaultLoader.
-    
-    let preventOnLoad = document.body.onload;
-    document.body.onload = function() {
-        preventOnLoad.call(this);
+    function OnLoad(args:Event) {
+        
+        if (preventOnLoad != null)
+            preventOnLoad(args);
+        
         document.body.style.display = '';
         document.body.onload = null;
     }
+    
+    document.body.onload = OnLoad;
 }
+
+//이렇게 했거든요.
