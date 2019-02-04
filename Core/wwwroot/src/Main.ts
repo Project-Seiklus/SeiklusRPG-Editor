1471b16196eb7daf7c6ec6fd7b31869f1675fdaa
Creta5164 <creta5164@gmail.com>
2019-02-04 19:35:43 +0900

    최상위 경로 가져오기 및 TSDoc 주석 추가

--- Core/wwwroot/src/Main.ts
+++ Core/wwwroot/src/Main.ts
@@ -4,15 +4,20 @@ export namespace SeiklusCore
 {
     export class Main {
         
+        /** RootUrl for resources of game. */
         private static RootUrl: string;
         
+        /** Current Phaser program. */
         private static Application: Phaser.Game;
         
-        public static Launch(url: string): void
+        /**
+         * Launch Seiklus core program.
+         * @param url The parent path of the game's resources.
+         */
+        public static Launch(url :string) :void
         {
-            Main.RootUrl = url;
+            Main.RootUrl = url.substring(0, url.lastIndexOf('/') + 1);
             
-            //현재 실행하고 있는 프로그램 종료
             if (Main.Application != null) {
                 Main.Application.destroy(true);
                 Main.Application = null;
@@ -25,8 +30,30 @@ export namespace SeiklusCore
                 
                 physics: {
                     default: 'arcade'
+                },
+                
+                scene: {
+                    preload: Main.OnPreload,
+                    create: Main.OnApplicationReady
                 }
+                
             });
+        }
+        
+        /**
+         * Phaser's preload method. it's called before Phaser has loaded.
+         */
+        private static OnPreload() :void
+        {
+            
+        }
+        
+        /**
+         * Phaser's create method. it's called after Phaser has loaded.
+         */
+        private static OnApplicationReady() :void
+        {
+            
         }
     }
     
